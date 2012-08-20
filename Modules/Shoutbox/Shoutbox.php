a1ccdefda930465dc7ac97b852286cb71f1398eb
Sandeep Jain <jainsandeep88@gmail.com>
2012-08-20 12:56:45 +0530

    Added Twitter, Shoutbox front-end modules

---
+++ Modules/Shoutbox/Shoutbox.php
@@ -0,0 +1,60 @@
+<?php
+class Project_Module_Shoutbox extends Curry_Module {
+
+    protected $title = '';
+
+    protected $messages = array();
+
+    public function showFront(Curry_Twig_Template $template=null){
+
+        $messages = Curry_Array::objectsToArray($this->messages, null);
+
+        return $template->render(array(
+            'Messages'=> $messages,
+            'Title' => $this->title,
+        ));
+    }
+
+    public function showBack(){
+        $messageForm = new Curry_Form_Dynamic(array(
+        'legend' => 'Entry',
+            'elements' => array(
+                'title' => array('text',array(
+                    'label' => 'Title',
+                    'required' => true,
+                )),
+                'text' => array('tinyMCE',array(
+                    'label' => 'Text',
+                    'required' => true,
+                )),
+            )
+        ));
+
+        $form = new Curry_Form_SubForm(array(
+            'elements' => array(
+                'title' => array('text', array(
+                    'label' => 'Shoutbox Title',
+                    'required' => true,
+                    'value' => $this->title,
+                )),
+            ),
+        ));
+
+
+        $form->addSubForm(new Curry_Form_MultiForm(array(
+            'legend' => 'Shoutbox Messages',
+            'cloneTarget' => $messageForm,
+            'defaults' => $this->messages
+        )), 'messages');
+
+        return $form;
+    }
+
+
+    public function saveBack(Zend_Form_SubForm $form) {
+        $values = $form->getValues(true);
+        $this->messages = $values['messages'];
+        $this->title = $values['title'];
+        $this->saveModule();
+    }
+}
---
+++ Modules/TwitterWidget/TwitterWidget.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Twitter Feed
+ *
+ */
+class Project_Module_TwitterWidget extends Curry_Module {
+
+    protected $title='';
+    protected $twitter_id='';
+
+    public function showFront(Curry_Twig_Template $template = null) {
+        return $template->render(array(
+            'Title' => $this->title,
+            'TwitterId'	=> $this->twitter_id,
+        ));
+    }
+
+    public function showBack() {
+        $form = new Curry_Form_SubForm(array(
+            'elements' => array(
+                'title' => array('text', array(
+                    'label' => 'Widget Title',
+                    'required' => true,
+                    'value' => $this->title,
+                )),
+                'twitter_id' => array('text', array(
+                    'label' => 'Twitter Id @',
+                    'required' => false,
+                    'description' => 'Your twitter username without \'@\' mark',
+                    'value' => $this->twitter_id,
+                )),
+            ),
+        ));
+
+        return $form;
+    }
+
+    public function saveBack(Zend_Form_SubForm $form) {
+        $values = $form->getValues(true);
+        $this->title = $values['title'];
+        $this->twitter_id = $values['twitter_id'];
+        $this->saveModule();
+    }
+
+} //endclass
